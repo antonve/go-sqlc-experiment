f8d59033df761c1a668a257db5524d155ab15df0
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-05-09 15:04:49 +0900

    init sqlc

---
+++ query.sql
@@ -0,0 +1,12 @@
+-- name: GetRestaurants :many
+select *
+from restaurants
+order by name;
+
+-- name: CreateRestaurant :one
+insert into restaurants (
+  name, location
+) values (
+  $1, $2
+)
+returning $1;
