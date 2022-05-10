db97fca05de63c6684b1f34d9e5ff584c3933ea5
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-05-10 11:04:40 +0900

    add more restaurants

--- example_test.go
+++ example_test.go
@@ -16,7 +16,7 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-func ExampleSqlc() {
+func ExampleQueries() {
 	ctx := context.Background()
 
 	psql, err := sql.Open("pgx", "host=postgis user=root dbname=experiment password=hunter2 sslmode=disable")
@@ -30,16 +30,19 @@ func ExampleSqlc() {
 
 	// create restaurants
 	cocoId := createRestaurant(queries, "CoCo Ichibanya Ebisu", 35.64699825984844, 139.71194575396922)
+	shakeId := createRestaurant(queries, "SHAKE SHACK Ebisu", 35.64669248211187, 139.70949784477963)
+	ichiranId := createRestaurant(queries, "Ichiran Ramen Shinjuku", 35.69079988476277, 139.70286473414785)
+	torikiId := createRestaurant(queries, "Torikizoku Shinjuku", 35.68918337273537, 139.70249991934935)
 
 	// list all restaurants
 	restaurants, err := queries.ListRestaurants(ctx)
 	if err != nil {
 		log.Fatalf("cannot list all restaurants: %v", err)
 	}
-	fmt.Println(cocoId, restaurants)
+	fmt.Println(cocoId, shakeId, ichiranId, torikiId, restaurants)
 
 	// Output:
-	// 1 [{1 CoCo Ichibanya Ebisu 0101000020E6100000194FC7D6D0D241405D3A7642C8766140}]
+	// 1 2 3 4 [{1 CoCo Ichibanya Ebisu 0101000020E6100000194FC7D6D0D241405D3A7642C8766140} {3 Ichiran Ramen Shinjuku 0101000020E61000007E9170216CD84140A8D52EDE7D766140} {2 SHAKE SHACK Ebisu 0101000020E6100000A99EBAD1C6D2414027FDD234B4766140} {4 Torikizoku Shinjuku 0101000020E6100000366C272937D841408A611CE17A766140}]
 }
 
 func createRestaurant(queries *db.Queries, name string, long, lat float64) int64 {
